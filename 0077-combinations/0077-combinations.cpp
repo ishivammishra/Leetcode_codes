e18da90abf94c7af934b71635c51fba197a228c4
Shivam Mishra <134406346+ishivammishra@users.noreply.github.com>
2025-05-27 16:20:38 +0530

    Time: 67 ms (45.57%), Space: 110 MB (25.12%) - LeetHub

--- 0077-combinations/0077-combinations.cpp
+++ 0077-combinations/0077-combinations.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    void solve(int i, int n, int k, vector<int>& op, vector<vector<int>>& res) {
-        if (op.size() == k) {
-            res.push_back(op);
-            return;
-        }
-        if (i > n) {
-            return;
-        }
-
-        op.push_back(i);
-        solve(i + 1, n, k, op, res);
-        op.pop_back();
-
-        solve(i + 1, n, k, op, res);
-    }
-
     vector<vector<int>> combine(int n, int k) {
         vector<int> op;
-        vector<vector<int>> res;
-        solve(1, n, k, op, res);
-        return res;
+        vector<vector<int>> ans;
+        solve(1, n, k, op, ans);
+        return ans;
+    }
+
+    void solve(int i, int n, int k, vector<int>& op, vector<vector<int>>& ans) {
+
+        if (op.size() == k) {
+            ans.push_back(op);
+            return;
+        }
+        if (i > n)
+            return;
+
+        op.push_back(i);
+        solve(i + 1, n, k, op, ans);
+        op.pop_back();
+        solve(i + 1, n, k, op, ans);
     }
 };
