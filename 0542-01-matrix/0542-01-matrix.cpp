82c5896b641720407ecb032fa96bb352de9409b7
Shivam Mishra <134406346+ishivammishra@users.noreply.github.com>
2025-06-05 21:03:36 +0530

    Time: 6 ms (95.47%), Space: 34.8 MB (78.37%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -7,33 +7,40 @@ public:
         vector<vector<int>> dist(m, vector<int>(n, -1));
         queue<pair<int, int>> q;
 
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                if (mat[i][j] == 0) {
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j<n; j++)
+            {
+                if(mat[i][j] == 0)
+                {
                     dist[i][j] = 0;
-                    q.push({i, j});
+                    q.push({i , j});
                 }
             }
         }
 
-        vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+        vector<pair<int , int >> directions = {{-1,0}, {1,0}, {0,-1}, {0,1}};
 
-        while (!q.empty()) {
+        while(!q.empty())
+        {
             auto curr = q.front();
             q.pop();
             int r = curr.first;
             int c = curr.second;
 
-            for (auto dir : directions) {
+            for(auto dir : directions)
+            {
                 int row = r + dir.first;
                 int col = c + dir.second;
 
-                if (row >= 0 and row < m and col >= 0 and col < n and dist[row][col] == -1) {
-                    dist[row][col] = dist[r][c] + 1;
+                if(row >= 0 and row < m and col >= 0 and col < n and dist[row][col] == -1)
+                {
+                    dist[row][col] = dist[r][c] +1;
                     q.push({row, col});
                 }
             }
         }
         return dist;
+
     }
 };
