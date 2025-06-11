7624fb2455f434c858693f9bf71a4d52a122c692
Shivam Mishra <134406346+ishivammishra@users.noreply.github.com>
2025-06-11 19:38:53 +0530

    Time: 2 ms (35.92%), Space: 16.8 MB (60.78%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.cpp
+++ 1036-rotting-oranges/1036-rotting-oranges.cpp
@@ -1,14 +1,17 @@
 class Solution {
 public:
+    int n, m;
+    vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+
     int orangesRotting(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        queue<pair<int, int>> q;
+        n = grid.size();
+        m = grid[0].size();
         int fresh = 0;
         int time = 0;
+        queue<pair<int, int>> q;
 
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
+        for (int r = 0; r < n; r++) {
+            for (int c = 0; c < m; c++) {
                 if (grid[r][c] == 1)
                     fresh++;
                 if (grid[r][c] == 2)
@@ -16,21 +19,19 @@ public:
             }
         }
 
-        vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
-
         while (fresh > 0 and !q.empty()) {
             int len = q.size();
             for (int i = 0; i < len; i++) {
-                auto curr = q.front();
+                auto current = q.front();
                 q.pop();
-                int r = curr.first;
-                int c = curr.second;
-
+                int r = current.first;
+                int c = current.second;
                 for (auto dir : directions) {
                     int row = r + dir.first;
                     int col = c + dir.second;
 
-                    if (row >= 0 and row < m and col >= 0 and col < n and grid[row][col] == 1) {
+                    if (row >= 0 and row < n and col >= 0 and col < m and
+                        grid[row][col] == 1) {
                         grid[row][col] = 2;
                         q.push({row, col});
                         fresh--;
@@ -39,6 +40,6 @@ public:
             }
             time++;
         }
-        return fresh == 0 ? time : -1;
+        return fresh == 0? time : -1;
     }
 };
