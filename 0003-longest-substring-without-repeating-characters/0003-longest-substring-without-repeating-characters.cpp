a14d85be447fc08593039ada2a80460403f29d49
Shivam Mishra <134406346+ishivammishra@users.noreply.github.com>
2025-07-08 11:10:50 +0530

    Time: 6 ms (70.54%), Space: 11.9 MB (48.3%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -6,15 +6,14 @@ public:
         int right = 0;
         int max_len = 0;
         unordered_map<char, int> mpp;
-
         while (right < n) {
             char c = s[right];
-
             if (mpp.find(c) != mpp.end() and mpp[c] >= left) {
                 left = mpp[c] + 1;
             }
+
             mpp[c] = right;
-            max_len = max(max_len, right - left + 1);
+            max_len = max(max_len, right-left + 1);
             right++;
         }
         return max_len;
