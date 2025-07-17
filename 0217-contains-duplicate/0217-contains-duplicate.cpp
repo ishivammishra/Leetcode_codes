ecfbbc4254098f2ad5d86a44530a39e2a1a499a0
Shivam Mishra <134406346+ishivammishra@users.noreply.github.com>
2025-07-17 11:26:40 +0530

    Time: 21 ms (89.84%), Space: 70.9 MB (95.9%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        ios_base::sync_with_stdio(false);
+        sort(nums.begin(), nums.end());
+
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1]) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
