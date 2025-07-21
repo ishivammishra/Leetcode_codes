b50a5ed02d703d0a2c5b91b55f44ce4541201e7a
Shivam Mishra <134406346+ishivammishra@users.noreply.github.com>
2025-07-21 20:10:54 +0530

    Time: 10 ms (88.83%), Space: 34.5 MB (94%) - LeetHub

--- 0542-01-matrix/0542-01-matrix.cpp
+++ 0542-01-matrix/0542-01-matrix.cpp
@@ -1,9 +1,11 @@
 class Solution {
 public:
     vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int n = mat.size();
         int m = mat[0].size();
+
         vector<vector<int>> dist(n, vector<int>(m, -1));
         queue<pair<int, int>> q;
         for (int r = 0; r < n; r++) {
@@ -18,14 +20,14 @@ public:
         while (!q.empty()) {
             auto curr = q.front();
             q.pop();
+
             int r = curr.first;
             int c = curr.second;
 
             for (auto dir : directions) {
                 int row = r + dir.first;
                 int col = c + dir.second;
-                if (row >= 0 and row < n and col >= 0 and col < m and
-                    dist[row][col] == -1) {
+                if (row >= 0 and row < n and col >= 0 and col < m and dist[row][col] == -1) {
                     dist[row][col] = dist[r][c] + 1;
                     q.push({row, col});
                 }
