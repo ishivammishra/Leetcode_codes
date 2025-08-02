6978dffbf54c4ba086e5bf5a637b5861e1634cf7
Shivam Mishra <134406346+ishivammishra@users.noreply.github.com>
2025-08-02 22:32:14 +0530

    Time: 3 ms (56.47%), Space: 28.8 MB (39.59%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.cpp
+++ 0695-max-area-of-island/0695-max-area-of-island.cpp
@@ -7,14 +7,14 @@ public:
             vector<vector<int>>& visit) {
         visit[r][c] = 1;
         int area = 1;
+
         for (auto dir : directions) {
             int row = r + dir.first;
             int col = c + dir.second;
 
             if (row >= 0 and row < n and col >= 0 and col < m and
-                grid[row][col] == 1 and !visit[row][col]) {
+                grid[row][col] == 1 and !visit[row][col])
                 area += dfs(row, col, grid, visit);
-            }
         }
         return area;
     }
@@ -22,9 +22,9 @@ public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         n = grid.size();
         m = grid[0].size();
-        int max_area = 0;
 
         vector<vector<int>> visit(n, vector<int>(m, 0));
+        int max_area = 0;
 
         for (int r = 0; r < n; r++) {
             for (int c = 0; c < m; c++) {
