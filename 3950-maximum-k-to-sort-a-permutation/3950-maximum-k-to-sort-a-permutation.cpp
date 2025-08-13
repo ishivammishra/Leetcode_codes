5c13360dde525a79dcca724a6c93f10cf4232b58
Shivam Mishra <134406346+ishivammishra@users.noreply.github.com>
2025-08-13 23:23:53 +0530

    Time: 5 ms (36.94%), Space: 152.7 MB (65.97%) - LeetHub

--- 3950-maximum-k-to-sort-a-permutation/3950-maximum-k-to-sort-a-permutation.cpp
+++ 3950-maximum-k-to-sort-a-permutation/3950-maximum-k-to-sort-a-permutation.cpp
@@ -3,22 +3,19 @@ public:
     int sortPermutation(vector<int>& nums) {
         int n = nums.size();
         int ans = -1;
-        for(int i = 0; i < n; i++)
-        {
-            if(nums[i] != i)
-            {
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i) {
                 ans = i;
                 break;
             }
         }
 
-        if(ans == -1) return 0;
-        for(int i = 0; i < n; i++)
-        {
-            if(nums[i] != i)
-            {
+        if (ans == -1)
+            return 0;
+
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i)
                 ans = ans & i;
-            }
         }
         return ans;
     }
