4bf4c3ba58a13cea5e945159223c36c3f137dcef
Shivam Mishra <134406346+ishivammishra@users.noreply.github.com>
2025-08-17 19:00:35 +0530

    Time: 0 ms (100%), Space: 14.8 MB (94.86%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.cpp
@@ -6,23 +6,20 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    
-    void solve(TreeNode* node, vector<int> &ans, int level)
-    {
-        if(node == NULL) return;
-
-        if(ans.size() == level) ans.push_back(node->val);
-
+    void solve(TreeNode* node, vector<int>& ans, int level) {
+        if (node == nullptr)
+            return;
+        if (ans.size() == level)
+            ans.push_back(node->val);
         solve(node->right, ans, level + 1);
         solve(node->left, ans, level + 1);
-
     }
-    
     vector<int> rightSideView(TreeNode* root) {
         vector<int> ans;
         solve(root, ans, 0);
