fe40287a04cf69e2bb0bfbbd5178b5c3b2baba97
Shivam Mishra <134406346+ishivammishra@users.noreply.github.com>
2025-08-18 16:49:24 +0530

    Time: 28 ms (37.58%), Space: 25.2 MB (60.55%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.cpp
@@ -2,38 +2,39 @@ class Solution {
 public:
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size();
-        if(grid[0][0] == 1 or grid[n-1][n-1] == 1) return -1;
+        if (grid[0][0] == 1 or grid[n - 1][n - 1] == 1)
+            return -1;
 
-        vector<pair<int,int>> directions = {{1,0},{-1,0},{0,1},{0,-1},{1,1},{1,-1},{-1,1},{-1,-1}};
+        vector<pair<int, int>> directions = {{1, 0},{-1, 0},{0, 1},
+                                             {0, -1},{1, 1},{1, -1},
+                                             {-1, 1}, {-1, -1}};
 
         queue<pair<int, int>> q;
-        vector<vector<int>> dist(n,vector<int>(n,-1));
+        vector<vector<int>> dist(n, vector<int>(n, -1));
 
-       q.push({0,0});
-       dist[0][0] = 1;
+        q.push({0, 0});
+        dist[0][0] = 1;
 
-       while(!q.empty())
-       {
+        while (!q.empty()) {
             auto curr = q.front();
             q.pop();
             int r = curr.first;
             int c = curr.second;
 
-            if(r == n-1 and c == n-1) return dist[r][c];
+            if (r == n - 1 and c == n - 1)
+                return dist[r][c];
 
-            for(auto dir : directions)
-            {
+            for (auto dir : directions) {
                 int row = r + dir.first;
                 int col = c + dir.second;
 
-                if(row>=0 and col >= 0 and row < n and col < n and grid[row][col] == 0 and dist[row][col] == -1)
-                {
+                if (row >= 0 and col >= 0 and row < n and col < n and
+                    grid[row][col] == 0 and dist[row][col] == -1) {
                     dist[row][col] = dist[r][c] + 1;
-                    q.push({row,col});
+                    q.push({row, col});
                 }
             }
-       }
-       return -1;
-
+        }
+        return -1;
     }
 };
