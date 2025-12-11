3dbe4e74bc6a32f5b1854aea7c442735da9df2b6
Shivam Mishra <134406346+ishivammishra@users.noreply.github.com>
2025-12-11 17:42:49 +0530

    Time: 0 ms (100%), Space: 21.1 MB (12.3%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
-    void dfs(int node, vector<int>& visited, vector<vector<int>>& adj) {
-        visited[node] = 1;
-        for (auto it : adj[node]) {
-            if (!visited[it]) {
-                dfs(it, visited, adj);
+    void dfs(int node, vector<int>& visit, vector<vector<int>>& adj) {
+
+        visit[node] = 1;
+        for (int it : adj[node]) {
+            if (!visit[it]) {
+                dfs(it, visit, adj);
             }
         }
     }
@@ -12,7 +13,6 @@ public:
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
 
-        // converting mmatrix into adj list
         vector<vector<int>> adj(n);
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -23,12 +23,12 @@ public:
             }
         }
 
-        vector<int> visited(n, 0);
-        int cnt=0;
+        vector<int> visit(n, 0);
+        int cnt = 0;
         for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
+            if (!visit[i]) {
                 cnt++;
-                dfs(i, visited, adj);
+                dfs(i, visit, adj);
             }
         }
         return cnt;
