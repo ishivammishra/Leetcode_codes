a5539df015006058556c862fe270ba358b24a6b1
Shivam Mishra <134406346+ishivammishra@users.noreply.github.com>
2025-12-29 12:55:05 +0530

    Time: 0 ms (100%), Space: 18 MB (73.64%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    int n, m;
-    vector<pair<int, int>> directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    int n,m;
+    vector<pair<int, int>> directions = {{-1,0},{0,1},{1,0},{0,-1}};
 
-    void dfs(int r, int c, vector<vector<int>>& image, int color,
-             int startColor) {
-        if (!(r >= 0 && r < n && c >= 0 && c < m && image[r][c] == startColor))
+    void dfs(int r, int c, vector<vector<int>>& image, int color, int start_col)
+    {
+        if(!(r >=0 and r < n and c >=0 and c < m and image[r][c] == start_col))
             return;
 
         image[r][c] = color;
 
-        for (const auto& dir : directions) {
+        for(const auto& dir : directions)
+        {
             int row = r + dir.first;
             int col = c + dir.second;
-            dfs(row, col, image, color, startColor);
+            dfs(row, col, image, color, start_col);
         }
     }
 
-    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc,
-                                  int color) {
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         n = image.size();
         m = image[0].size();
-        int startColor = image[sr][sc];
 
-        if (startColor == color)
+        int start_col = image[sr][sc];
+
+        if(start_col == color)
             return image;
 
-        dfs(sr, sc, image, color, startColor);
-        return image;
+        dfs(sr, sc, image, color, start_col);
+        return image;    
     }
 };
